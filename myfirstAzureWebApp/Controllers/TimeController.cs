ee9736a24ebb76627764ebf7714452e9d0e6cd2d
SUHONGJIAN <enactus2014shj@163.com>
2018-12-04 21:22:14 +0800

    ASP.NET源代码上传-4

---
+++ myfirstAzureWebApp/Controllers/TimeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myfirstAzureWebApp.Controllers
+{
+    public class TimeController : Controller
+    {
+        // GET: Time
+        public ActionResult Display()
+        {
+            //string currenttime = DateTime.Now.ToString();
+            return View();
+        }
+    }
+}
